4f23a5bfd81abb507363b8bf19ff93960fdd9701
Preston Haddock <quotablesarcasm@gmail.com>
2024-07-04 07:43:33 -0600

    "Nicer" end battle scene

--- Scenes/UI/EndBattleModal.gd
+++ Scenes/UI/EndBattleModal.gd
@@ -1,10 +1,25 @@
 extends Control
 class_name EndBattleModal
 
-var battleStats: Dictionary = {
-	"test": 123
+signal restart()
+
+var battleStatsText: Dictionary = {
+	"damage_dealt": "Damage Dealt",
+	"damage_taken": "Damage Taken",
+	"damage_healed": "Damage Healed",
+	"highest_hit": "Highest Hit",
+	"xp_gained": "XP Gained",
+	"xp_remaining": "XP to next Level"
 }
+var battleStats: Dictionary = {}
 
 func _ready():
 	for key in self.battleStats:
-		%BattleStatsList.add_item("%s: %d" % [key, self.battleStats[key]])
+		if self.battleStatsText.has(key):
+			%BattleStatsList.add_item("%s: %d" % [self.battleStatsText[key], self.battleStats[key]])
+
+	%GoAgain.connect("button_up", self._restart)
+
+func _restart() -> void:
+	queue_free()
+	restart.emit()
