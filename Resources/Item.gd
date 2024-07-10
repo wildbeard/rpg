bb655d24cdf3f03f927707e12e1c675b3a43676b
Preston Haddock <quotablesarcasm@gmail.com>
2024-07-10 11:24:32 -0600

    Added categorys to Item.gd

--- Resources/Item.gd
+++ Resources/Item.gd
@@ -3,8 +3,13 @@ class_name Item
 
 @export var texture: Texture
 @export var itemType: InventoryTypes.SlotType = InventoryTypes.SlotType.REGULAR
+
+@export_category("Basics")
 @export var name: String
-@export var description: String
+@export_multiline var description: String
+@export var price: int = 10
+
+@export_category("Quantity & Stacking")
 @export var quantity: int = 0:
 	set(value):
 		if !isStackable:
