5cb139b351313573c3dfebe90eac79ea06032554
Preston Haddock <quotablesarcasm@gmail.com>
2024-07-10 08:04:43 -0600

    Update the Item Resource to be more useful

--- Resources/Item.gd
+++ Resources/Item.gd
@@ -3,5 +3,13 @@ class_name Item
 
 @export var texture: Texture
 @export var itemType: InventoryTypes.SlotType = InventoryTypes.SlotType.REGULAR
-@export var quantity: int = 0
+@export var name: String
+@export var description: String
+@export var quantity: int = 0:
+	set(value):
+		if !isStackable:
+			quantity = 1
+		else:
+			quantity = value
+@export var maxStack: int = 99
 @export var isStackable: bool = false
