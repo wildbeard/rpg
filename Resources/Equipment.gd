0270db194c4edd2b2a0a6d3da26d804a220dcde5
Preston Haddock <quotablesarcasm@gmail.com>
2024-07-12 08:58:07 -0600

    Added stat modifiers to Equipment resources

--- Resources/Equipment.gd
+++ Resources/Equipment.gd
@@ -1,5 +1,14 @@
 extends Item
 class_name Equipment
 
+@export var statModifiers: Dictionary = {
+	"strength": 0,
+	"dexterity": 0,
+	"vitality": 0,
+	"wisdom": 0,
+	"intelligence": 0,
+	"speed": 0,
+}
+
 func equip() -> void:
 	pass
