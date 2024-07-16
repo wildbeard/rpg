22d3a9ea8b380ecde4c473e2ed26b8e3671f1b4d
Preston Haddock <quotablesarcasm@gmail.com>
2024-07-15 19:43:21 -0600

    A new pecking order

--- Resources/Item.gd
+++ Resources/Item.gd
@@ -1,6 +1,13 @@
 extends Resource
 class_name Item
 
+enum ItemRarity {
+	Common,
+	Rare,
+	Epic,
+	Legendary,
+}
+
 @export var texture: Texture
 @export var itemType: InventoryTypes.SlotType = InventoryTypes.SlotType.REGULAR
 
@@ -8,6 +15,7 @@ class_name Item
 @export var name: String
 @export_multiline var description: String
 @export var price: int = 10
+@export var rarity: ItemRarity
 
 @export_category("Stacking")
 @export var maxStack: int = 99
