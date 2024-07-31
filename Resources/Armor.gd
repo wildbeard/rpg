b5842b17a2f8ca9cc1ea76362c9ded9cf9271369
Preston Haddock <quotablesarcasm@gmail.com>
2024-07-31 12:51:09 -0600

    Added a simple mechanic for mitigating damage

--- Resources/Armor.gd
+++ Resources/Armor.gd
@@ -10,3 +10,19 @@ enum ArmorType {
 @export_enum("Light", "Medium", "Heavy") var armorType: int
 @export var physicalDefense: int = 1
 @export var magicalDefense: int = 1
+
+var _defenseWeight: Dictionary = {
+	ArmorType.LIGHT: 120.0,
+	ArmorType.MEDIUM: 110.0,
+	ArmorType.HEAVY: 95.0,
+}
+
+func _getItemDefenseWeight() -> float:
+	return self._defenseWeight[self.armorType]
+
+func getDamageReduction(physical: bool = true) -> float:
+	var armorVal: float = float(self.physicalDefense if physical else self.magicalDefense)
+	var weight: float = self._getItemDefenseWeight()
+	var mit: float = armorVal / (armorVal + weight)
+
+	return (armorVal / (armorVal + self._getItemDefenseWeight()))
