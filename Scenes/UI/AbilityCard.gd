7284e6f5ad3c044843cfd66bffb6abb5de30485a
Preston Haddock <quotablesarcasm@gmail.com>
2024-08-13 08:52:52 -0600

    Update AbilityCard logic for use in LevelUp

--- Scenes/UI/AbilityCard.gd
+++ Scenes/UI/AbilityCard.gd
@@ -1,4 +1,8 @@
 extends Control
+class_name AbilityCard
+
+signal select(ability: Ability)
+signal deselect()
 
 @export var ability: Ability
 
@@ -8,6 +12,9 @@ extends Control
 @onready var ability_element = $AbilityElement
 @onready var cooldown = $Cooldown
 
+var _hovered: bool = false
+var _selected: bool = false
+
 func _ready() -> void:
 	self.ability_name.text = ability.name
 	self.ability_icon.texture = self.ability.icon
@@ -15,3 +22,28 @@ func _ready() -> void:
 	self.ability_description.text = self.ability.getDescription(PlayerManager.player)
 	self.ability_element.text = "Element: %s" % self.ability.getElementType()
 	self.cooldown.text = "CD: %d" % self.ability.cooldown
+
+	self.connect("mouse_entered", func(): self._mouseEvent(true))
+	self.connect("mouse_exited", func(): self._mouseEvent(false))
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton \
+		&& self._hovered \
+		&& event.button_index == MOUSE_BUTTON_LEFT:
+			self.handleFocus()
+
+func _mouseEvent(isHovered: bool) -> void:
+	self._hovered = isHovered
+
+func handleFocus() -> void:
+	var t: Tween = create_tween()
+	t.tween_property(self, "scale", Vector2(1.025, 1.025), 0.25)
+	self.select.emit(self.ability)
+	self._selected = true
+
+func handleExit(emitEvent: bool = true) -> void:
+	var t: Tween = create_tween()
+	t.tween_property(self, "scale", Vector2(1, 1), 0.25)
+
+	if emitEvent:
+		self.deselect.emit()
